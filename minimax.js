c192172b5908371677b263e9989cb09d005a89f5
NemesLaszlo <wow.laszlo@gmail.com>
2020-01-11 10:42:44 +0100

    TicTacToe implementation with AI - Minimax

--- minimax.js
+++ minimax.js
@@ -1,15 +1,31 @@
-# TicTacToe_AI_Minimax
-Simple Tic Tac Toe game on 3x3 table with a Minimax algorithm implementation. Using Vanilla Javascript and p5.js for visualization and practise.
+function bestMove() {
+  // AI to make its turn
+  let bestScore = -Infinity;
+  let move;
+  for (let i = 0; i < 3; i++) {
+    for (let j = 0; j < 3; j++) {
+      // Is the spot available?
+      if (board[i][j] == '') {
+        board[i][j] = ai;
+        let score = minimax(board, 0, false);
+        board[i][j] = '';
+        if (score > bestScore) {
+          bestScore = score;
+          move = { i, j };
+        }
+      }
+    }
+  }
+  board[move.i][move.j] = ai;
+  currentPlayer = human;
+}
 
-#### Source links and others:
-https://en.wikipedia.org/wiki/Minimax
+let scores = {
+  X: 10,
+  O: -10,
+  pass: 0
+};
 
-https://www.ntu.edu.sg/home/ehchua/programming/java/JavaGame_TicTacToe_AI.html
-
-
-##### Minimax function:
-
-```javascript
 function minimax(board, depth, isMaximizing) {
   let result = checkWinner();
   if (result !== null) {
@@ -46,4 +62,3 @@ function minimax(board, depth, isMaximizing) {
     return bestScore;
   }
 }
-```
